87d0f0f9a6d5e30f35958c53f584bbe4ce761efe
Levi <84652664+levirenato@users.noreply.github.com>
2022-12-21 10:15:08 -0300

    Add files via upload

--- base.py
+++ base.py
@@ -6,90 +6,83 @@ from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import dash_bootstrap_components as dbc
 
+####  Importações de bases de dados  ####
+
+#GeoJson (Fonte IBGE)
+response = requests.get(url='https://servicodados.ibge.gov.br/api/v3/malhas/estados/26?formato=application/vnd.geo+json&qualidade=maxima&intrarregiao=municipio')    
+if response.status_code == 200:
+    geojson = response.json()
+
+#Base De Dados (Com resumo por municipio)
+df = pd.read_csv('RESUMO.CSV',sep=';')
+
 # Div Dashboard
-app = Dash(external_stylesheets=[dbc.themes.CYBORG])
+app = Dash(__name__,external_stylesheets=[dbc.themes.LUX])
 
 app.layout = html.Div([
-    html.H3('Estatisticas de Covid', style={"font-famiy":"'Source Sans Pro', 'sans-serif'",
-                                            "text-align": "center",
-                                            "background-color":"#2780e3",
-                                            "color":"white",
-                                            "margin-top":"0px"}),
+    #Head
+    dbc.NavbarSimple([
     
+    dbc.RadioItems(
+        className="btn-group",
+        inputClassName="btn-check",
+        labelClassName="btn btn-outline-primary",
+        labelCheckedClassName="active",
+        id='offcanvas-placement-selector',
+        options = [
+                {"label": "Morbidade", "value": "Morbidade"},
+                {"label": "Confirmados", "value": "Confirmados"},
+                {"label": "Recuperados", "value":"Recuperados"},
+                {"label": "Obitos", "value":"Obitos"}], value="Morbidade",
+        )
+    ],brand='Estatisticas de Covid'),
+    
+    # Cards
     dbc.Row([
         dbc.Col([
             dbc.Card([
                 dbc.CardBody([
-                    html.Span("Casos Recuperados",className="card-text"),
-                    html.H3(style={"color":"#e7e9eb"}, id="casos-recuperados")
+                    html.H4("Casos Recuperados",className="card-title"),
+                    html.P(df["Recuperados"].sum(),id="casos-recuperados",className="card-text")
                 ])
-            ],color='#555555',outline=True,style={"margin":"10px"})
+            ],outline=True,style={"margin":"10px","text-align":"center"})
         ], md=4),
         
         dbc.Col([
             dbc.Card([
                 dbc.CardBody([
-                    html.Span("Casos Confirmados",className="card-text"),
-                    html.H3(style={"color":"#e7e9eb"}, id="casos-confirmados")
+                    html.H4("Casos Confirmados",className="card-title"),
+                    html.P( df["Confirmados"].sum(),id="casos-confirmados",className="card-text")
                 ])
-            ],color='#555555',outline=True,style={"margin":"10px"})
+            ],outline=True,style={"margin":"10px","text-align":"center"})
         ], md=4),
         
         dbc.Col([
             dbc.Card([
                 dbc.CardBody([
-                    html.Span("Casos de Óbito",className="card-text"),
-                    html.H3(style={"color":"#e7e9eb"}, id="obito")
+                    html.H4("Casos de Óbito",className="card-title"),
+                    html.P( df["Obitos"].sum(),id="obitos",className="card-text")
                 ])
-            ],color='#555555',outline=True,style={"margin":"10px"})
+            ],outline=True,style={"margin":"10px","text-align":"center"})
         ], md=4)
         
     ]),
-    html.P("Selecione uma Categoria:"),
-    html.Div(
-    [dcc.RadioItems(
-        id='offcanvas-placement-selector', 
-        options=["Morbidade","Confirmados", "Recuperados", "Obitos"],
-        value="Morbidade",
-        inline=True,
-        
-    )],style={"margin-rigth":"5%"}
-    ,className="mb-2"),
-    dcc.Graph(id="graph"),
-    dcc.Graph(id="top"),
+   
+   #Graficos
+    html.Div([ 
+        dcc.Graph(id="graph",style={"margin-top":"2%"}),
+        dcc.Graph(id="top",style={"margin-top":"2%"})
+    ])
     
-    
-])
+], style={"padding-right":"3%","padding-left":"3%"})
 
-# Interactivity
-@app.callback(
-    [
-        Output("casos-recuperados", "children"),
-        Output("obito", "children"),
-        Output("casos-confirmados", "children"),
-    ], [Input("offcanvas-placement-selector", "value")])
-def display_status(categoria):
-    recuperados = f'{int(categoria["Recuperados"].sum):,}'.replace(",", ".") 
-    confirmados = f'{int(categoria["Confirmados"].sum):,}'.replace(",", ".") 
-    obitos = f'{int(categoria["Obitos"].sum):,}'.replace(",", ".")
-    
-    return(recuperados,confirmados,obitos)
+
 
 # MAP function with the choice filter
 @app.callback(
     Output("graph", "figure"),
     Input("offcanvas-placement-selector", "value"))
-def display_choropleth(categoria):
-    ####  Importações de bases de dados  ####
-
-    #GeoJson (Fonte IBGE)
-    response = requests.get(url='https://servicodados.ibge.gov.br/api/v3/malhas/estados/26?formato=application/vnd.geo+json&qualidade=maxima&intrarregiao=municipio')    
-    if response.status_code == 200:
-        geojson = response.json()
-
-    #Base De Dados (Com resumo por municipio)
-    df = pd.read_csv('RESUMO.CSV',sep=';')
-    
+def display_choropleth(categoria):    
     #Grafico Do Mapa
     
     # condição para definir o tema
@@ -101,9 +94,8 @@ def display_choropleth(categoria):
         tema = 'darkmint'
     else: tema = 'greys'
     
-    fig = px.choropleth_mapbox(df, geojson=geojson, color=categoria,
-                            locations="codarea", featureidkey="properties.codarea",color_continuous_scale=tema,
-                            center={"lat":  -8.27519, "lon": -38.0376},mapbox_style="carto-positron", zoom=7,
+    fig = px.choropleth_mapbox(df, geojson=geojson, color=categoria,locations="codarea", featureidkey="properties.codarea",color_continuous_scale=tema,
+                            center={"lat":  -8.27519, "lon": -38.0376},mapbox_style="carto-positron", zoom=6.6,title="Mapa Taxa de {}".format(categoria),
                             hover_data={"Municipio":True,"codarea":False, "Morbidade":True, "Confirmados":True,"Recuperados":True,"Obitos":True})
 
 
@@ -117,26 +109,23 @@ def display_choropleth(categoria):
 @app.callback(
 Output("top","figure"),
 Input("offcanvas-placement-selector", "value"))
-def top_gra(categoria):
-    ####  Importações de bases de dados  ####
-
-    #GeoJson (Fonte IBGE)
-    response = requests.get(url='https://servicodados.ibge.gov.br/api/v3/malhas/estados/26?formato=application/vnd.geo+json&qualidade=maxima&intrarregiao=municipio')    
-    if response.status_code == 200:
-        geojson = response.json()
-
-    #Base De Dados (Com resumo por municipio)
-    df = pd.read_csv('RESUMO.CSV',sep=';')
-    
+def top_gra(categoria):  
     #top 10
-    top10 = df.nlargest(n=10, columns=['{}'.format(categoria)])
+    top10 = df.nlargest(n=6, columns=['{}'.format(categoria)])
     fig = px.bar(df, x=top10['Municipio'],
                      y=top10['{}'.format(categoria)],
-                     color=top10['Municipio'], title='10 Cidades com maior {}'.format(categoria))
+                     color=top10['Municipio'], title='6 Cidades com maior {}'.format(categoria))
     fig.update_layout(
     margin=dict(l=10, r=10, t=30, b=10)
 )
     return fig
-    
+
+#Click
+
+
+
+
+      
+      
 # Run Aplication
 app.run_server(debug=True)
